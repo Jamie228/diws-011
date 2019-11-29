0db4fd3e5b9c4b12f4266c068a0f3f65c4ba012c
Jamie Hamilton (P110103851) <P110103851@student.peterborough.ac.uk>
2019-11-29 12:03:39 +0000

    Attempt to integrate bootstrap with react

--- app/src/App.js
+++ app/src/App.js
@@ -3,10 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 import Heading from './components/Heading';
 import TextInput from './components/TextInput';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <header className="App-header">
       <Heading color='#ff00ff'>Welcome</Heading>
         <img src={logo} className="App-logo" alt="logo" />
