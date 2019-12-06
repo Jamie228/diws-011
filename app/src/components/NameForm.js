fae5ef6754fe5b4f1561e07638b62300fb19bf6e
Jamie Hamilton (P110103851) <P110103851@student.peterborough.ac.uk>
2019-12-06 11:36:02 +0000

    Edits to React app

--- app/src/components/NameForm.js
+++ app/src/components/NameForm.js
@@ -1,14 +1,26 @@
-import { useState } from 'react';
-import React from 'react';
-
+import { useState, useEffect } from "react";
+import React from "react";
 
 export function NameForm(props) {
-    const [name, setName] = useState("");
-    
-    const handleSubmit = (evt) => {
-        evt.preventDefault();
-    }
-    return (
+  const [name, setName] = useState("");
+  const [done, setDone] = useState(false);
+  const [courseName, setCourseName] = useState("");
+
+  const handleBscClick = evt => {
+    setCourseName("bsc");
+  }
+
+  const handleSubmit = evt => {
+    evt.preventDefault();
+    setDone(true);
+  };
+
+  useEffect(() => {
+    console.log("name: ", name);
+  }, [name]);
+
+  return (
+    <>
       <form onSubmit={handleSubmit}>
         <label>
           <input
@@ -20,7 +32,18 @@ export function NameForm(props) {
         </label>
         <button className="btn btn-primary mx-3">Submit</button>
       </form>
-    );
-  }
+      {done ? (
+        <div className="container">
+          <br></br>
+          <h2>Hello {name}</h2>
+          <p>Thanks for checking out UCP, {name}. It means a lot! Now, what course are you interested in?</p>
+            <button onClick = {handleBscClick} type="button" className="btn btn-primary btn-lg btn-block">BSc (Hons)</button>
+            <button type="button" className="btn btn-primary btn-lg btn-block">FdSc</button>
+        </div>
+      ) : null}
+      
+    </>
+  );
+}
 
-  export default NameForm;
+export default NameForm;
--- app/src/components/NavBar.js
+++ app/src/components/NavBar.js
@@ -1,55 +1,84 @@
-import React from 'react';
-import logo from './images/UCPLogo.png';
+import React from "react";
+import logo from "./images/UCPLogo.png";
 
 const imgStyle = {
-  height: '50px'
+  height: "50px"
 };
 
-export function NavBar(){
-
-    return(
-      <nav className="navbar navbar-expand-sm bg-light navbar-light">
+export function NavBar() {
+  return (
+    <nav className="navbar navbar-expand-sm bg-light navbar-light">
       <a className="navbar-brand" href="#">
         <img src={logo} alt="logo" style={imgStyle} />
       </a>
       <span className="navbar-brand mb-0 h1" href="#">
         <strong>Computing</strong>
       </span>
-  
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
-        aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNavDropdown"
+        aria-controls="navbarNavDropdown"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
         <span className="navbar-toggler-icon"></span>
       </button>
-  
+
       <div className="collapse navbar-collapse" id="navbarNavDropdown">
         <ul className="navbar-nav">
           <li className="nav-item">
-            <a className="nav-link" href="#">Home<span className="sr-only">(current)</span></a>
+            <a className="nav-link" href="localhost:5500/index.html">
+              Home<span className="sr-only">(current)</span>
+            </a>
           </li>
           <li className="nav-item dropdown">
-            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown"
-              aria-haspopup="true" aria-expanded="false">
+            <a
+              className="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDropdownMenuLink"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
               Course Information
             </a>
-            <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-              <a className="dropdown-item" href="bscInfo.html">BSc (Hons)</a>
-              <a className="dropdown-item" href="fdscInfo.html">FdSc</a>
+            <div
+              className="dropdown-menu"
+              aria-labelledby="navbarDropdownMenuLink"
+            >
+              <a className="dropdown-item" href="bscInfo.html">
+                BSc (Hons)
+              </a>
+              <a className="dropdown-item" href="fdscInfo.html">
+                FdSc
+              </a>
             </div>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="moduleExplorer.html">Module Explorer</a>
+            <a className="nav-link" href="moduleExplorer.html">
+              Module Explorer
+            </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="calendar.html">Events Calendar</a>
+            <a className="nav-link" href="calendar.html">
+              Events Calendar
+            </a>
           </li>
           <li className="nav-item active">
-            <a className="nav-link" href="http://localhost:3000/">Virtual Open Day <span
-                className="badge badge-secondary">New</span><span className="sr-only">(current)</span></a>
+            <a className="nav-link" href="http://localhost:3000/">
+              Virtual Open Day{" "}
+              <span className="badge badge-secondary">New</span>
+              <span className="sr-only">(current)</span>
+            </a>
           </li>
         </ul>
       </div>
     </nav>
-    );
+  );
 }
 
 export default NavBar;
